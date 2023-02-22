e7ccc87ab7a67c41364f7123af8f1b0ba69f1f3e
ankit sharma <ankit8008990@gmail.com>
2023-02-22 10:23:00 +0000

    Changes has been made by Krutika

--- src/list_interfacekrutika/List_Interface.java
+++ src/list_interfacekrutika/List_Interface.java
@@ -4,27 +4,30 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-
-
-
 public class List_Interface {
-	
-	public static void main (String[]args) {
+		public static void main (String[]args) {
 		
 	List<Integer> grade = new ArrayList<Integer>();
 	grade.add(80);
 	grade.add(85);
 	grade.add(70);
 	grade.add(80);
+	List<Integer> grade1 = new ArrayList<Integer>();
+	grade.add(80);
+	grade.add(85);
+	grade.add(70);
+	grade.add(80);
 	
 	for (Integer student : grade) {
-		System.out.println("We want grades for all students " + grade);
-		}
-	//we can also do via Iterator
-	Iterator<Integer>myint = grade.iterator();
-	while (myint.hasNext()) {
-	System.out.println("We are iterating grade here via Iterator "+ myint.hasNext());
-	}
-
+		System.out.println("We want grades for all students " + grade);}
+		
+	System.out.println("************************************************");
+	grade.addAll(grade);
+	System.out.println(grade);// It allows duplicate value
+	System.out.println(grade.contains(85)); //can chk conatins function //true//
+	System.out.println(grade.equals(72)); //can compare same number is there or not //false//
+	System.out.println(grade.equals(70));//false//
+    
+	
 }
 }
