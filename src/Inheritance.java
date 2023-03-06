7ced55e1b3e3847d54a9cc8fc38ca0e399b5ec65
VaghelaSushma <swinkuvaghela@gmail.com>
2023-03-06 14:59:57 +0000

    my changes

---
+++ src/Inheritance.java
@@ -0,0 +1,13 @@
+
+public class Inheritance {
+
+	/* Why java does not have multiple inheritance?
+	 * 
+	 * Answer: Java does not have multiple inheritance because of ambiguity problem. In case of multiple inheritance, 
+	 * a single class is derived from two or more parent classes. So, there may be a possibility that two or more
+	 * parent classes have a member function with the same name. If the object of child class needs to access one of
+	 * the same named member function from the parent class, then it results in ambiguity. The compiler is confused as 
+	 * method of which parent class method to invoke on executing the call statement. */
+	 
+	
+}
---
+++ src/Student.java
@@ -0,0 +1,16 @@
+
+public class Student {
+	
+	
+	//this is parent class
+	
+	int id;
+	
+	
+	public  void display_id() {
+		
+		System.out.println("The id of the student is "+id);
+		
+	}
+
+}
